b86d6702028d7cf8d419649966889c5b6d1ddd6d
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2025-02-16 19:47:14 +0100

    Fixed y-sorting issue and removed "reserved_tiles" variable

--- src/global_scripts/global_variables.gd
+++ src/global_scripts/global_variables.gd
@@ -1,6 +1,2 @@
 extends RefCounted
 class_name GlobalVar
-
-
-## Stores the tiles that are currently occupied by NPCs. A safer alternative to Godot's collision handling system.
-static var reserved_tiles : Array[Vector2i]
