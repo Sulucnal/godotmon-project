f5556c941ef12861277810e2e2a758ecc734d337
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2025-04-05 19:58:17 +0200

    Updated the Constants class to match with the wiki

--- src/global_scripts/global_constants.gd
+++ src/global_scripts/global_constants.gd
@@ -16,59 +16,55 @@ const TILE_SIZE : int = 16
 
 #region MONSTERS
 
-## The genders monsters can have. Affect their reproduction as well as the effects of some moves.
-enum GENDER { MALE, FEMALE, GENDERLESS }
-## The likeliness of a monster being male or female.[br]
-enum GENDER_RATIO { ALWAYS_MALE, FEMALE_ONE_EIGHTH, FEMALE_25_PERENT, FEMALE_50_PERCENT, FEMALE_75_PERCENT, FEMALE_SEVEN_EIGHTHS, ALWAYS_FEMALE, GENDERLESS}
+## Enum describing all the possible gender generation rates.
+enum FemaleRate {
+ASEXUAL,
+MALE_ONLY,
+FEMALE_12_5,
+FEMALE_25,
+FEMALE_37_5,
+FEMALE_62_5,
+FEMALE_75,
+FEMALE_87_5,
+FEMALE_ONLY
+}
 
 #endregion
 
 #region MOVES
 
-## The entity affected by a specific move.
-enum MOVE_TARGET {
-	## Every monster on the battlefield.
-	ALL,
+## Enum describing which target a move can reach.
+enum AttackTarget {
+	## The user of the move.
+	SELF,
+	## A monster close to the user.
+	ONE_ADJACENT,
 	## Every monster on the battlefield except for the user.
-	ALL_ADJACENT,
-	## Every monster on the opposing side of the battlefield.
-	ALL_ADJACENT_FOES,
-	## Every monster on the user's side of the battlefield.
-	ALL_ALLIES,
-	## Every monster in the user's party.
-	ALLY_TEAM,
-	## Affects the battlefield itself, on both side.
-	FIELD,
-	## Affects the battlefield on the opposing side.
-	FOE_SIDE,
-	## Affects the battlefield on the user's side.
-	USER_SIDE,
+	EVERYONE_ASIDE_SELF,
+	## Every monster on the battlefield.
+	EVERYONE,
+	## Every monster in the opposing party.
+	ENEMYBANK,
+	## Every monster in the party of the tamer of the user, except for the user itself.
+	ALLY_BANK_ASIDE_SELF,
+	## Every monster in the party of the tamer of the user, including the user itself.
+	ALLY_BANK,
 	## A random monster close to the user.
 	RANDOM_ADJACENT,
-	## A monster close to the user.
-	SINGLE_ADJACENT,
-	## A monster close to the user and on its side.
-	SINGLE_ADJACENT_ALLY,
-	## A monster close to the user and on the opposing side.
-	SINGLE_ADJACENT_FOE,
-	## A monster on the user's side including the user itself.
-	SINGLE_ALLY_OR_USER,
-	## A monster that isn't the user.
-	SINGLE_BUT_USER,
-	## The user of the move.
-	USER,
+	## A random monster that isn't the user.
+	RANDOM_ASIDE_SELF
 }
 
 ## Each move comes with one of the following categories. They are used in the damage calculation of
 ## an attack.
-enum MOVE_CATEGORY {
+enum MoveCategory {
 	## Damaging moves where the user makes direct contact with the target like a punch or a bite.
 	DIRECT,
 	## Damaging moves where the user affects the target without being physically in contact with it
 	## like with a projectile or a laser beam.
 	INDIRECT,
 	## Moves where the target doesn't take damage.
-	OTHER,
+	OTHER
 }
 
 #endregion
