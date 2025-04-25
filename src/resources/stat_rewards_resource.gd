f3abea558ad99ebfb268b9372a5214ceed019a1e
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2025-04-25 18:19:49 +0200

    Added the stat_rewards_resource.gd script (#16)

---
+++ src/resources/stat_rewards_resource.gd
@@ -0,0 +1,23 @@
+extends Resource
+class_name StatRewards
+
+
+## All of the stat points that can be added to stats when defeating a monster.
+##
+## @tutorial: https://github.com/Maruno17/godotmon-project/wiki/Data-types#statrewards
+
+
+## Health points added when defeating this monster.
+@export var hp : int
+## Physical Attack points added when defeating this monster.
+@export var physical_attack : int
+## Physical Defence points added when defeating this monster.
+@export var physical_defence : int
+## Special Attack points added when defeating this monster.
+@export var special_attack : int
+## Special Defense points added when defeating this monster.
+@export var special_defence : int
+## Speed points added when defeating this monster.
+@export var speed : int
+## Loyalty added when defeating this monster.
+@export var loyalty : int
