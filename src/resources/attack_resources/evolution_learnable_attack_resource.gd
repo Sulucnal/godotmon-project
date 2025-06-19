0309afce473006a7b6f91a6538fc112a8e516b4f
Sulucnal <100075515+Sulucnal@users.noreply.github.com>
2025-06-19 14:10:00 +0200

    Added AttackData resources and renamed folder with attack resources related scripts.

--- src/resources/attack_resources/evolution_learnable_attack_resource.gd
+++ src/resources/attack_resources/evolution_learnable_attack_resource.gd
@@ -2,6 +2,6 @@ extends LearnableAttack
 class_name EvolutionLearnableAttack
 
 
-##Data entity describing an Attack that is learnt when evolving to this Monster.
+## Data entity describing an Attack that is learnt when evolving to this Monster.
 ##
 ## @tutorial: https://github.com/Maruno17/godotmon-project/wiki/Data-types#evolutionlearnableattack-extends-learnableattack
